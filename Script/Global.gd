5984f4d634f648c9593de112a96ba68f78d48652
22npatrick <22npatrick@wakatipu.school.nz>
2026-06-18 13:51:08 +1200

    Can now immediately retry using the enter key

--- Script/Global.gd
+++ Script/Global.gd
@@ -2,3 +2,7 @@ extends Node
 
 var score = 0
 var high_score = 0
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("enter"):
+		get_tree().change_scene_to_file("res://Scene/main_1.tscn")
