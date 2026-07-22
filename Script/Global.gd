225431b066537f22ed3092a026d7c57a3bcc99dd
22npatrick <22npatrick@wakatipu.school.nz>
2026-07-22 13:26:59 +1200

    Obstacles are now the things that move
    Instead of having both the floor and the obstacles conjoined together obstacles now move on their own.

--- Script/Global.gd
+++ Script/Global.gd
@@ -1,10 +1,10 @@
 extends Node
-
 var score = 0
 var high_score = 0
 var speed_increase = 1.03
 var max_speed = 1.5
 var sec = 0
-func _process(delta: float) -> void:
+
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("enter"):
 		get_tree().change_scene_to_file("res://Scene/main_1.tscn")
--- Script/player_1.gd
+++ Script/player_1.gd
@@ -13,19 +13,13 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y =JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("left", "right")
-	#if direction:
-	#	velocity.x = direction * SPEED
-	#else:
-	#	velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	# COLIISION DETECTION
+		
 	move_and_slide()
+	
 	var collision = move_and_collide(velocity * delta)
-	for i in get_slide_collision_count():
-		collision = get_slide_collision(i)
-		if collision.get_collider().is_in_group("obstacles"):
-			get_tree().call_deferred("change_scene_to_packed", preload("res://Scene/end_game_scene.tscn"))
+	#for i in get_slide_collision_count():
+		#collision = get_slide_collision(i)
+		#if collision.get_collider().is_in_group("obstacles"):
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("yes")
+	get_tree().call_deferred("change_scene_to_packed", preload("res://Scene/end_game_scene.tscn"))
