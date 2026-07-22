bd459aa9e06ded3804cd7a233124a1b3cfb036ea
22npatrick <22npatrick@wakatipu.school.nz>
2026-07-22 21:48:05 +1200

    obstacles are now randomly spaced

--- Script/player_1.gd
+++ Script/player_1.gd
@@ -22,15 +22,12 @@ func _physics_process(delta: float) -> void:
 		scale = Vector2(1, 1)
 		print("up")
 		compressed = false
-		
-		
 	move_and_slide()
-	
-	var collision = move_and_collide(velocity * delta)
+	var _collision = move_and_collide(velocity * delta)
 	#for i in get_slide_collision_count():
 		#collision = get_slide_collision(i)
 		#if collision.get_collider().is_in_group("obstacles"):
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
+func _on_area_2d_area_entered(_area: Area2D) -> void:
 	print("yes")
 	get_tree().call_deferred("change_scene_to_packed", preload("res://Scene/end_game_scene.tscn"))
