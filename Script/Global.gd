212fc08f9062a1d06f31fce8de07455b584995f8
22npatrick <22npatrick@wakatipu.school.nz>
2026-07-22 23:24:47 +1200

    fixed issue where the difficulty settings was not displayed correctly

--- Script/Global.gd
+++ Script/Global.gd
@@ -2,9 +2,10 @@ extends Node
 var speed = 400.0
 var score = 0
 var high_score = 0
-var speed_increase = 1.03
+var speed_increase = 1.0075
 var max_speed = 500
 var sec = 0
+var game_diff = [true, false, false]
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("enter"):
--- Script/player_1.gd
+++ Script/player_1.gd
@@ -2,31 +2,43 @@ extends CharacterBody2D
 
 
 const SPEED = 100
-const JUMP_VELOCITY = -500
+const JUMP_VELOCITY = -450
 var compressed = false
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("up") and is_on_floor() and compressed == false:
-		velocity.y =JUMP_VELOCITY
-	if Input.is_action_just_pressed("down"):
-		compressed = true
-		print("down")
-		scale = Vector2(1, 0.5)
-	if Input.is_action_just_released("down"):
-		scale = Vector2(1, 1)
-		print("up")
-		compressed = false
+	if self == $"../Player 2":
+		if Input.is_action_just_pressed("ui_up") and compressed == false:
+			velocity.y = -1000
+		if Input.is_action_just_pressed("ui_down"):
+			compressed = true
+			print("down")
+			scale = Vector2(1, 0.5)
+			velocity.y = 1000
+		if Input.is_action_just_released("ui_down"):
+			scale = Vector2(1, 1)
+			print("up")
+			velocity.y = 0
+			compressed = false
+	else:
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+		# Handle jump.
+		if Input.is_action_just_pressed("up1") and is_on_floor() and compressed == false:
+			velocity.y =JUMP_VELOCITY
+		if Input.is_action_just_pressed("down1"):
+			compressed = true
+			print("down")
+			scale = Vector2(1, 0.5)
+		if Input.is_action_just_released("down1"):
+			scale = Vector2(1, 1)
+			print("up")
+			compressed = false
 	move_and_slide()
 	var _collision = move_and_collide(velocity * delta)
-	#for i in get_slide_collision_count():
-		#collision = get_slide_collision(i)
-		#if collision.get_collider().is_in_group("obstacles"):
+		#for i in get_slide_collision_count():
+			#collision = get_slide_collision(i)
+			#if collision.get_collider().is_in_group("obstacles"):
 
 func _on_area_2d_area_entered(_area: Area2D) -> void:
 	print("yes")
