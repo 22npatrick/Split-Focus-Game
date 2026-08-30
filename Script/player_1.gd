743e5fd1f02a383acb96c459e4c381752a418913
22npatrick <22npatrick@wakatipu.school.nz>
2026-08-31 03:23:58 +1200

    Final Version
    Minor changes due to feedback. This is the final version of the project.

--- Script/player_1.gd
+++ Script/player_1.gd
@@ -7,17 +7,15 @@ var compressed = false
 
 func _physics_process(delta: float) -> void:
 	if self == $"../Player 2":
-		if Input.is_action_just_pressed("ui_up") and compressed == false:
+		if Input.is_action_just_pressed("up1") and compressed == false:
 			velocity.y = JUMP_VELOCITY
-		if Input.is_action_just_pressed("ui_down") and compressed == false:
+		if Input.is_action_just_pressed("down1") and compressed == false:
 			compressed = true
-			print("down")
 			scale = Vector2(1, 0.5)
 			velocity.y = 100
-		if Input.is_action_just_released("ui_down")and compressed == true:
+		if Input.is_action_just_released("down1")and compressed == true:
 			compressed = false
 			scale = Vector2(1, 1)
-			print("up")
 		velocity += get_gravity() * delta/5
 	else:
 		if not is_on_floor():
@@ -28,16 +26,14 @@ func _physics_process(delta: float) -> void:
 			$Sprite2D.pause()
 			$Sprite2D.play("Idle")
 		# Handle jump.
-		if Input.is_action_just_pressed("up1") and is_on_floor() and compressed == false:
+		if Input.is_action_just_pressed("ui_up") and is_on_floor() and compressed == false:
 			velocity.y =JUMP_VELOCITY
 		#
-		if Input.is_action_just_pressed("down1"):
+		if Input.is_action_just_pressed("ui_down"):
 			compressed = true
-			print("down")
 			scale = Vector2(1, 0.5)
-		if Input.is_action_just_released("down1"):
+		if Input.is_action_just_released("ui_down"):
 			scale = Vector2(1, 1)
-			print("up")
 			compressed = false
 	move_and_slide()
 	var _collision = move_and_collide(velocity * delta)
@@ -46,16 +42,12 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	if self == $"../Player 2":
 		if area.is_in_group("birdob"):
 			get_tree().call_deferred("change_scene_to_packed", preload("res://Scene/end_game_scene.tscn"))
-			print("Game end player 2")
 		if area.is_in_group("kangob"):
 			Global.score += 5	
 			Global.gather_correct_ob_1 = true
-			print("Player 2 collect ob 1")
 	else:
 		if area.is_in_group("kangob"):
 			get_tree().call_deferred("change_scene_to_packed", preload("res://Scene/end_game_scene.tscn"))
-			print("Game end player 1")
 		if area.is_in_group("birdob"):
 			Global.score += 5
 			Global.gather_correct_ob_2 = true
-			print("Player 1 collect ob 2")
